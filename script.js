54612a8e92ee2964084ef731087a66aababfcf15
Griselda Mendieta <87496972+mendixta@users.noreply.github.com>
2021-10-01 19:58:30 -0500

    adding the js and css files

---
+++ script.js
@@ -0,0 +1,10 @@
+// function to open and close the slide-overlay
+function openNav(){
+    document.getElementById("projects-nav").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("projects-nav").style.width = "0%";
+}
+
+
